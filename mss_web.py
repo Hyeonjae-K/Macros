c24c76c111b753ac9da0a8daf7ee635617be3ddd
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-11-11 19:41:22 +0900

    FIX - find_element_by 함수 대체

--- mss_web.py
+++ mss_web.py
@@ -1,42 +1,48 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
 
 my_id = 'id'
 my_pw = 'pw'
 purchase_time = ['10시', '00분', '00초']
 url = 'url'
+user_agent = 'user-agent'
 options = ['op1', 'op2']
 
-options = Options()
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-options.add_argument('headless')
-options.add_argument('user-agent=%s' % user_agent)
-time_driver = webdriver.Chrome()
+ops = Options()
+ops.add_argument('headless')
+ops.add_argument('window-size=1920x1080')
+ops.add_argument('user-agent=%s' % user_agent)
+time_driver = webdriver.Chrome(chrome_options=ops)
+
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
 
 def login():
     driver.get(
         'https://my.musinsa.com/login/v1/login?referer=https%3A%2F%2Fstore.musinsa.com%2Fapp%2F')
-    driver.find_element_by_css_selector(
-        'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > input:nth-child(5)').send_keys(my_id)
-    driver.find_element_by_css_selector(
-        'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > input:nth-child(6)').send_keys(my_pw)
-    driver.find_element_by_css_selector(
-        'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > button').click()
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > input:nth-child(5)'))).send_keys(my_id)
+    driver.find_element(
+        By.CSS_SELECTOR, 'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > input:nth-child(6)').send_keys(my_pw)
+    driver.find_element(
+        By.CSS_SELECTOR, 'body > div.musinsa-wrapper.wrapper-member.devicePC > div.n-member-area > form > button').click()
     driver.get(url)
 
 
 def checkTime():
     time_driver.get('https://time.navyism.com/?host=store.musinsa.com')
-    time_driver.find_element_by_css_selector(
-        '#navyismMainContainer > div:nth-child(15) > div:nth-child(1) > div:nth-child(1) > label:nth-child(2)').click()
-    time_driver.find_element_by_css_selector(
-        '#navyismMainContainer > div:nth-child(15) > div:nth-child(1) > div:nth-child(2) > label:nth-child(4)').click()
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#navyismMainContainer > div:nth-child(15) > div:nth-child(1) > div:nth-child(1) > label:nth-child(2)'))).click()
+    time_driver.find_element(
+        By.CSS_SELECTOR, '#navyismMainContainer > div:nth-child(15) > div:nth-child(1) > div:nth-child(2) > label:nth-child(4)').click()
     while True:
-        server_time = time_driver.find_element_by_css_selector(
-            '#time_area').text.split()[3:]
+        server_time = time_driver.find_element(
+            By.CSS_SELECTOR, '#time_area').text.split()[3:]
         if server_time == purchase_time:
             driver.get(url)
             break
@@ -44,49 +50,25 @@ def checkTime():
 
 def purchase():
     for i in range(len(options)):
-        ops = driver.find_elements_by_css_selector(
-            '#option%d > option' % (i+1))
+        ops = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '#option%d > option' % (i+1))))
         for op in ops:
             if options[i] in op.text:
                 op.click()
                 break
-    driver.find_element_by_css_selector(
-        '#product_order_info > div.explan_product.option_select_section.opt-select-box > div.box-btn-buy.wrap-btn-buy > div.btn_buy > a').send_keys(Keys.ENTER)
-    while True:
-        try:
-            driver.find_element_by_css_selector(
-                '#payment_info_area > ul:nth-child(9) > li.cell_discount_detail.last > p:nth-child(1) > label > span').click()
-        except:
-            continue
-        else:
-            break
-    driver.find_element_by_css_selector(
-        '#btn_pay').send_keys(Keys.ENTER)
-    while True:
-        try:
-            driver.switch_to.frame(
-                driver.find_element_by_css_selector('#naxIfr'))
-        except:
-            continue
-        else:
-            break
-    while True:
-        try:
-            driver.find_element_by_css_selector(
-                '#frmObj > div.content > div.con > div.agree_new.form_row > div.agree_chk_bg > div.chk_box.chk_agree_all > label').click()
-        except:
-            continue
-        else:
-            break
-    driver.find_element_by_css_selector('#spayNext').click()
-    while True:
-        try:
-            driver.find_element_by_css_selector(
-                '#frmObj > div.content > div.con > div:nth-child(5) > div.chk_box.chk_cash.ma_t20 > label').click()
-        except:
-            continue
-        else:
-            break
+    driver.find_element(
+        By.CSS_SELECTOR, '#product_order_info > div.explan_product.option_select_section.opt-select-box > div.box-btn-buy.wrap-btn-buy > div.btn_buy > a').send_keys(Keys.ENTER)
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#payment_info_area > ul:nth-child(9) > li.cell_discount_detail.last > p:nth-child(1) > label > span'))).click()
+    driver.find_element(By.CSS_SELECTOR, '#btn_pay').send_keys(Keys.ENTER)
+    driver.switch_to.frame(wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#naxIfr'))))
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#frmObj > div.content > div.con > div.agree_new.form_row > div.agree_chk_bg > div.chk_box.chk_agree_all > label'))).click()
+    driver.find_element(By.CSS_SELECTOR, '#spayNext').click()
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#frmObj > div.content > div.con > div:nth-child(5) > div.chk_box.chk_cash.ma_t20 > label'))).click()
+    driver.find_element_by_css_selector('#spayNext').click
 
 
 login()
