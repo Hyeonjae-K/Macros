399ee56ba009109d15a9609723d14a2383fe45c0
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2021-12-18 13:18:21 +0900

    FEAT - 전역 변수 및 이벤트 함수 추가

---
+++ Nike_SNKRS/background.js
@@ -0,0 +1,20 @@
+let g_process = 0;
+let g_size = null;
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === 'get_status') {
+    if (g_process){
+      g_process++;
+    }
+    sendResponse([g_process, g_size]);
+  }
+});
+
+function runFunction(input_size) {
+  g_size = input_size;
+  g_process = 1;
+}
+
+function resetFunction() {
+  g_process = 0;
+}
